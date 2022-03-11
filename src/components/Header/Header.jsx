313e975db83d72473e03263d880b80c652d63ddd
Justin Lam <justin.kh.lam@gmail.com>
2022-03-10 19:48:31 -0500

    add in app.js logic to render default page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import styles from './Header.module.css';
+
 const Header = () => (
     <h1 className={styles.heading}>
         <span className={styles.light}>Weather</span> Forecast
