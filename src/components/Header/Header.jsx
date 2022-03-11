4558a01045fa516dc4569d7f085b00978c36396e
Justin Lam <justin.kh.lam@gmail.com>
2022-03-10 19:45:21 -0500

    add in Header base component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+    <h1 className={styles.heading}>
+        <span className={styles.light}>Weather</span> Forecast
+    </h1>
+);
+
+export default Header;
