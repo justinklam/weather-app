bf548213ff1acecc3652faf758125fe62d14d2b2
Justin Lam <justin.kh.lam@gmail.com>
2022-03-13 13:52:47 -0400

    add in cross_domain and request_URL variables in useForecast hook

--- src/hooks/useForecast.js
+++ src/hooks/useForecast.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import axios from "axios";
 
 const BASE_URL = 'https://www.metaweather.com/api/location'
+const CROSS_DOMAIN = 'https://the-ultimate-api-challenge.herokuapp.com'
+const REQUEST_URL = `${CROSS_DOMAIN}/${BASE_URL}`
 
 const useForecast = () => {
   const [isError, setError] = useState(false);
