807d6374cfb71abb93c120675e20b04a77da1f2e
Justin Lam <justin.kh.lam@gmail.com>
2022-03-17 15:01:01 -0400

    call currentdaydescription component

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -10,26 +10,27 @@ import UpcomingDaysForecast from '../UpcomingDaysForecast';
 import styles from './Forecast.module.css';
 
 const Forecast = ({ forecast }) => (
-  <Container className={styles.box}>
-      <Row>
-          <Col xs={12} md={4}>
-              <div className={styles.card}>
-                <CurrentDay {...forecast.currentDay} />
-              </div>
-          </Col>
-          <Col xs={12} md={8} className="d-flex flex-column justify-content-between">
-            <UpcomingDaysForecast days={forecast.upcomingDays} />
-          </Col>
-      </Row>
-  </Container>
+    <Container className={styles.box}>
+        <Row>
+            <Col xs={12} md={4}>
+                <div className={styles.card}>
+                    <CurrentDay {...forecast.currentDay} />
+                </div>
+            </Col>
+            <Col xs={12} md={8} className="d-flex flex-column justify-content-between">
+                <CurrentDayDescription forecast={forecast.currentDayDetails} />
+                <UpcomingDaysForecast days={forecast.upcomingDays} />
+            </Col>
+        </Row>
+    </Container>
 );
 
 Forecast.propTypes = {
-  forecast: PropTypes.shape({
-    currentDay: PropTypes.object,
-    currentDayDetails: PropTypes.array,
-    upcomingDays: PropTypes.array
-  }),
+    forecast: PropTypes.shape({
+        currentDay: PropTypes.object,
+        currentDayDetails: PropTypes.array,
+        upcomingDays: PropTypes.array,
+    }),
 };
 
 export default Forecast;
