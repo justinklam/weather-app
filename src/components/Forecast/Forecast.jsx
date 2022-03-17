10ba5b84f4d8e9dc50de77580b4de81c8f859292
Justin Lam <justin.kh.lam@gmail.com>
2022-03-16 23:58:48 -0400

    add in UpComingDaysForecast component into forecast.jsx

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -15,6 +15,7 @@ const Forecast = ({ forecast }) => (
           <Col xs={12} md={4}>
               <div className={styles.card}>
                   <CurrentDay {...forecast.currentDay} />
+                  <UpcomingDaysForecast days={forecast.upcomingDays} />
               </div>
           </Col>
           <Col xs={12} md={8} className="d-flex flex-column justify-content-between"></Col>
--- src/components/UpcomingDaysForecast/UpcomingDaysForecast.jsx
+++ src/components/UpcomingDaysForecast/UpcomingDaysForecast.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 
 import styles from './UpcomingDaysForecast.module.css';
 
-const UpcomingDaysForecast = () => <ul className={`${styles.weekList} d-flex justify-content-between p-0`}></ul>;
+const UpcomingDaysForecast = ({days}) => <ul className={`${styles.weekList} d-flex justify-content-between p-0`}></ul>;
 
 export default UpcomingDaysForecast;
