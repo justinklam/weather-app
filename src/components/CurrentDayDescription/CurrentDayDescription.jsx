dcfc96362302ea00675f43b6edd01b8eebfa7dc5
Justin Lam <justin.kh.lam@gmail.com>
2022-03-16 23:49:03 -0400

    add a loop into CurrentDayDescription that iterates through CurrentDayDescriptionItem. pass in props into CurrentDayDescriptionItem

--- src/components/CurrentDayDescription/CurrentDayDescription.jsx
+++ src/components/CurrentDayDescription/CurrentDayDescription.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import CurrentDayDescriptionItem from '../CurrentDayDescriptionItem'
+
 const CurrentDayDescription = ({forecast}) => (
     <div className="mt-4 mt-md-2">
-        <div className="d-flex flex-column mb-2"></div>
+        <div className="d-flex flex-column mb-2">
+          {forecast.map(() => {
+            <CurrentDayDescriptionItem {...item} key={item.name}/>
+          })}
+        </div>
     </div>
 );
 
--- src/components/CurrentDayDescriptionItem/CurrentDayDescriptionItem.jsx
+++ src/components/CurrentDayDescriptionItem/CurrentDayDescriptionItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const CurrentDayDescriptionItem = () => (
+const CurrentDayDescriptionItem = ({name, value, unit}) => (
     <div className="d-flex justify-content-between">
         <p className="mb-0 font-weight-bolder text-uppercase"></p>
         <p className="mb-0"></p>
