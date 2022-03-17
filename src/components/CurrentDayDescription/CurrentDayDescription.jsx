a74a38d6d14fa18ceff904e24bd154f802b3851b
Justin Lam <justin.kh.lam@gmail.com>
2022-03-16 23:46:34 -0400

    add in props and propTypes into currentDayDescription

--- src/components/CurrentDayDescription/CurrentDayDescription.jsx
+++ src/components/CurrentDayDescription/CurrentDayDescription.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const CurrentDayDescription = () => (
+const CurrentDayDescription = ({forecast}) => (
     <div className="mt-4 mt-md-2">
         <div className="d-flex flex-column mb-2"></div>
     </div>
 );
+
+CurrentDayDescription.propTypes = {
+  forecast: PropTypes.array
+};
 
 export default CurrentDayDescription;
