e8d87bdbecc2986f37b9eab57dc57ad54333b58c
Justin Lam <justin.kh.lam@gmail.com>
2022-03-17 14:31:49 -0400

    format change for Forecast.jsx component. pass in props for UpcomingDaysForecastItem

--- src/components/Forecast/Forecast.jsx
+++ src/components/Forecast/Forecast.jsx
@@ -14,11 +14,12 @@ const Forecast = ({ forecast }) => (
       <Row>
           <Col xs={12} md={4}>
               <div className={styles.card}>
-                  <CurrentDay {...forecast.currentDay} />
-                  <UpcomingDaysForecast days={forecast.upcomingDays} />
+                <CurrentDay {...forecast.currentDay} />
               </div>
           </Col>
-          <Col xs={12} md={8} className="d-flex flex-column justify-content-between"></Col>
+          <Col xs={12} md={8} className="d-flex flex-column justify-content-between">
+            <UpcomingDaysForecast days={forecast.upcomingDays} />
+          </Col>
       </Row>
   </Container>
 );
--- src/components/UpcomingDaysForecastItem/UpcomingDaysForecastItem.jsx
+++ src/components/UpcomingDaysForecastItem/UpcomingDaysForecastItem.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import styles from './UpcomingDaysForecastItem.module.css';
 
-const UpcomingDaysForecastItem = () => (
+const UpcomingDaysForecastItem = ({weekday, temperature, imgURL}) => (
     <li className={`${styles.weekday} d-flex flex-column justify-content-center align-items-center p-2`}></li>
 );
 
