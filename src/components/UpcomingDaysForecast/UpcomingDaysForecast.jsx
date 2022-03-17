b47101318aadc21e82b3f2623f6ef54bbd3e034d
Justin Lam <justin.kh.lam@gmail.com>
2022-03-17 00:02:11 -0400

    add in logic for UpcomingDaysForecast to take in UpcomingDaysForecastItem

--- src/components/UpcomingDaysForecast/UpcomingDaysForecast.jsx
+++ src/components/UpcomingDaysForecast/UpcomingDaysForecast.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 
 import styles from './UpcomingDaysForecast.module.css';
 
-const UpcomingDaysForecast = ({days}) => <ul className={`${styles.weekList} d-flex justify-content-between p-0`}></ul>;
+import UpcomingDaysForecastItem from '../UpcomingDaysForecastItem';
+
+const UpcomingDaysForecast = ({days}) => <ul className={`${styles.weekList} d-flex justify-content-between p-0`}>
+  {days.map((day) => {
+    <UpcomingDaysForecastItem {...day} key={day.weekday} />
+  })}
+</ul>;
 
 export default UpcomingDaysForecast;
