07bbd4cf6e8bd8b2579dda18b300b51a17d65efc
Justin Lam <justin.kh.lam@gmail.com>
2022-03-16 23:52:28 -0400

    correct loop logic in CurrentDayDescription. add in logic to display value/unit

--- src/components/CurrentDayDescription/CurrentDayDescription.jsx
+++ src/components/CurrentDayDescription/CurrentDayDescription.jsx
@@ -6,7 +6,7 @@ import CurrentDayDescriptionItem from '../CurrentDayDescriptionItem'
 const CurrentDayDescription = ({forecast}) => (
     <div className="mt-4 mt-md-2">
         <div className="d-flex flex-column mb-2">
-          {forecast.map(() => {
+          {forecast.map((item) => {
             <CurrentDayDescriptionItem {...item} key={item.name}/>
           })}
         </div>
