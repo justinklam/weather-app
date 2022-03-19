48bdf058beee542e1580da25eb68022fd8dbd69b
Justin Lam <justin.kh.lam@gmail.com>
2022-03-18 23:46:54 -0400

    add in propTypes

--- src/components/UpcomingDaysForecast/UpcomingDaysForecast.jsx
+++ src/components/UpcomingDaysForecast/UpcomingDaysForecast.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import UpcomingDaysForecastItem from '../UpcomingDaysForecastItem';
 
@@ -11,5 +12,9 @@ const UpcomingDaysForecast = ({ days }) => (
         ))}
     </ul>
 );
+
+UpcomingDaysForecast.propTypes = {
+    days: PropTypes.array.isRequired,
+};
 
 export default UpcomingDaysForecast;
